9fd40e5fa39a34bc9b77d5c604ae1f9c905b1fc2
Manelich1107 <164870708+Manelich1107@users.noreply.github.com>
2025-08-21 02:23:28 -0400

    fix hypnos world bug

---
+++ HypnosWorld.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+//using StructureHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Terraria;
+using Terraria.ID;
+using Terraria.DataStructures;
+using Terraria.GameContent.Generation;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using Terraria.WorldBuilding;
+
+namespace HypnosMod
+{
+    public class HypnosWorld : ModSystem
+    {
+		public static bool downedHypnos = false;
+
+		
+		public static void UpdateWorldBool()
+		{
+			if (Main.netMode == NetmodeID.Server)
+			{
+				NetMessage.SendData(MessageID.WorldData);
+			}
+		}
+		public override void OnWorldLoad()
+		{
+			downedHypnos = false;
+		}
+		public override void OnWorldUnload()
+		{
+			downedHypnos = false;
+		}
+		public override void SaveWorldData(TagCompound tag)
+		{
+            tag["downedHypnos"] = downedHypnos;
+		}
+
+		public override void LoadWorldData(TagCompound tag)
+		{
+			// In new worlds the key may not exist; default to false.
+			downedHypnos = tag.ContainsKey("downedHypnos") ? tag.Get<bool>("downedHypnos") : false;
+		}
+		public override void NetSend(BinaryWriter writer)
+		{
+            writer.Write(downedHypnos);
+		}
+		public override void NetReceive(BinaryReader reader)
+		{
+            downedHypnos = reader.ReadBoolean();
+		}
+	}
+}
